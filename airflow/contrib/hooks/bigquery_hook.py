1a68813367bbdefac6f9c405f1eb63d05477a1d4
fenglu-g <fenglu@google.com>
2018-01-25 10:35:27 -0800

    [AIRFLOW-2029] Fix AttributeError in BigQueryPandasConnector
    Closes #2971 from fenglu-g/master
    
    (cherry picked from commit bfbdeca6536ddcccc3c53ce980b5999f10eed69b)

--- airflow/contrib/hooks/bigquery_hook.py
+++ airflow/contrib/hooks/bigquery_hook.py
@@ -133,6 +133,7 @@ class BigQueryPandasConnector(GbqConnector):
     service account credentials into the binding.
     """
     def __init__(self, project_id, service, reauth=False, verbose=False, dialect='legacy'):
+        super(BigQueryPandasConnector, self).__init__(project_id)
         gbq_check_google_client_version()
         gbq_test_google_api_imports()
         self.project_id = project_id
